81f222e4a3205e14199c8923f0291376090e43c1
ZocoPDF <paulopaulopaulopaulo@gmail.com>
2023-05-02 00:07:35 +0200

    cambios menores

--- LectorDeWeb.py
+++ LectorDeWeb.py
@@ -6,11 +6,10 @@ import requests
 class LectorDeWeb():
     def __init__(self, url="https://www.movistarplus.es/programacion-tv", timeout=1) -> None:
         self.response = requests.get(url)
-    
+
     def __str__(self):
         return self.response.text
 
 if __name__ == "__main__":
     lector = LectorDeWeb()
-    print (lector.response.headers)
-
+    print (type(lector.response.content))
