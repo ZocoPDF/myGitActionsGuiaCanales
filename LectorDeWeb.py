308057642b13b4c15d91955d51bfadf374ac3548
ZocoPDF <paulopaulopaulopaulo@gmail.com>
2023-05-03 20:47:14 +0200

    dejo 1 channel para ver cuál es el que detecta

--- LectorDeWeb.py
+++ LectorDeWeb.py
@@ -4,12 +4,17 @@ import requests
 
 
 class LectorDeWeb():
-    def __init__(self, url="https://www.movistarplus.es/programacion-tv", timeout=1) -> None:
+    def __init__(self, url="https://www.movistarplus.es/programacion-tv", timeout=3) -> None:
         self.response = requests.get(url)
 
     def __str__(self):
         return self.response.text
 
 if __name__ == "__main__":
-    lector = LectorDeWeb()
-    print (type(lector.response.content))
+    datosxml="<programme><title></title></programme>"
+    # lector = LectorDeWeb()
+    # print(type(lector.response.text))
+    # print(lector)
+    # print (type(lector.response.content))
+    prueba = ET.fromstring(datosxml)
+    print(type(prueba))
