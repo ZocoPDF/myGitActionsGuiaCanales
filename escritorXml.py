72b62720aafd66a80c689df3e35db0157e41e131
ZocoPDF <paulopaulopaulopaulo@gmail.com>
2023-05-21 22:39:33 +0200

    7ª beta quito timezone +2 a +0, intento resolver sobra de canales en dicc canales

--- escritorXml.py
+++ escritorXml.py
@@ -19,15 +19,20 @@ class EscritorXml():
             displayName = ET.SubElement(channel, "display-name")
             displayName.text = self.titulosCanales[self.canales[canal]]
 
+        canalesM3uAlmacenados=[]
         # agregamos los elementos <programme>
         for canal in self.programacion:
-            canalM3u = self.buscaKeyPorCanal(canal) # el problema con esto es que nunca llega a la 2ª v que encuentra
+            canalM3u = self.buscaKeyPorCanal(canal, canalesM3uAlmacenados)
+            if canalM3u == None: 
+                canalM3u = canal # el problema con esto es que nunca llega a la 2ª v que encuentra
+            else:
+                canalesM3uAlmacenados.append(canalM3u)
             for programa in self.programacion[canal]:
                 programme = ET.SubElement(root, "programme", attrib={
                     'start':self.convertirFechas(self.programacion[canal][programa]['horaInicio']),
                     'stop':self.convertirPixelsEnFechas(self.programacion[canal][programa]['horaInicio'],
                                                         self.programacion[canal][programa]['horaFin']),
-                    'channel':canalM3u if canalM3u != None else canal 
+                    'channel':canalM3u
 
                     })
                 titulo = ET.SubElement(programme, "title")
@@ -45,14 +50,14 @@ class EscritorXml():
     # 'hh:mm' -> "YYYYMMDDHHMMSS + Zona Horaria"
     def convertirFechas(self, hora:str):
         hoyFecha = datetime.now().date()
-        fechaHora = datetime.combine(hoyFecha, datetime.strptime(hora, "%H:%M").time(), tzinfo=timezone(timedelta(hours=2)))
+        fechaHora = datetime.combine(hoyFecha, datetime.strptime(hora, "%H:%M").time(), tzinfo=timezone(timedelta(hours=0)))
         return fechaHora.strftime("%Y%m%d%H%M%S %z")
 
     # obtener hora fin en formato hh:mm a partir de la hora de inicio y duración en pixels, 20/100 min/pixel
     def convertirPixelsEnFechas(self, hora:str, pixels:str):
         minutos = int(pixels)*20/100
         hoyFecha = datetime.now().date()
-        fechaHora = datetime.combine(hoyFecha, datetime.strptime(hora, "%H:%M").time(), tzinfo=timezone(timedelta(hours=2)))
+        fechaHora = datetime.combine(hoyFecha, datetime.strptime(hora, "%H:%M").time(), tzinfo=timezone(timedelta(hours=0)))
         fechaHoraFin = fechaHora + timedelta(minutes=minutos)
         return fechaHoraFin.strftime("%Y%m%d%H%M%S %z")
     
@@ -69,7 +74,7 @@ class EscritorXml():
         return prettyText
     
     # busca un value en un dict, pero ojo, no
-    def buscaKeyPorCanal(self, canal):
+    def buscaKeyPorCanal(self, canal, canalesM3uYaVistos):
         for k, v in self.canales.items():
-            if v in canal:return k
+            if v in canal and k not in canalesM3uYaVistos:return k
         return None
